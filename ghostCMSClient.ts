be0b3d9ed46919ffe07298b8078cdf786cb52384
Airshyre <airshyre@protonmail.com>
2021-12-24 21:14:40 +0000

    Use environment variables.

---
+++ ghostCMSClient.ts
@@ -0,0 +1,8 @@
+
+import GhostContentAPI from '@tryghost/content-api'
+
+export const ghostClient = new GhostContentAPI({ 
+    url: process.env.GHOST_API_URL || "", 
+    key: process.env.GHOST_API_CONTENT_KEY || "", 
+    version: 'v3'
+});
