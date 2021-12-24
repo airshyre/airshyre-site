bd822eff507692718e237868ae82d0ab1f685e4b
Airshyre <airshyre@protonmail.com>
2021-12-24 21:45:15 +0000

    Add comma.

--- ghostCMSClient.ts
+++ ghostCMSClient.ts
@@ -4,5 +4,5 @@ import GhostContentAPI from '@tryghost/content-api'
 export const ghostClient = new GhostContentAPI({ 
     url: process.env.GHOST_API_URL || "", 
     key: process.env.GHOST_API_CONTENT_KEY || "", 
-    version: 'v3'
+    version: 'v3',
 });
