2d6e43c61e27da3c89b8bf529da91c91108e4d12
airshyre <airshyre@protonmail.com>
2021-12-29 16:38:08 +0000

    Set default tab index.

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -2,5 +2,5 @@ import { ComponentProps } from "react"
 
 
 export const ExternalLink = ({children, ...props}: ComponentProps<"a">) => {
-    return <a rel="noopen noreferrer" target="_blank" {...props}>{children}</a>
+    return <a rel="noopen noreferrer" target="_blank" tabIndex={0} {...props}>{children}</a>
 }
